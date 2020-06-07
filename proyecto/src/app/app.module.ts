75316416b0d875dfdfd77a2dc1b87f04d37cec22
Crisal95 <cristian.alvarado@ucr.ac.cr>
2020-06-07 17:44:05 -0600

    pequeño cambio

--- proyecto/src/app/app.module.ts
+++ proyecto/src/app/app.module.ts
@@ -5,11 +5,7 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {LoginComponent} from './login/login.component';
 import {HomeComponent} from './home/home.component';
-<<<<<<< HEAD
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-=======
-import {ReactiveFormsModule, FormsModule} from '@angular/forms';
->>>>>>> master
 import {UserService} from './shared/user.service';
 import {RouteGuard} from './shared/route-guard';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
